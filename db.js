7de18891477a1f28e67877dc72d51d3aeff70491
kkupschi <kkupschi@gmail.com>
2025-11-19 12:58:41 +0100

    First Build

---
+++ db.js
@@ -0,0 +1,27 @@
+let dishes = [
+    {
+        name: "Spaghetti Carbonara",
+        price: 12.99,
+        description: "Cremige Pasta mit Speck und Parmesan, verfeinert mit ...",
+        category: "Hauptgerichte",
+        image: "Logo/carbonara.png"
+    },
+    {
+        name: "Vegetarische Lasagne",
+        price: 10.50,
+        description: "Herzhafte Lasagne mit Schichten aus Gemüse, Tomatensauce...",
+        category: "Hauptgerichte"
+    },
+    {
+        name: "Gegrilltes Hähnchen mit Quinoa",
+        price: 14.20,
+        description: "Saftiges Hähnchenbrustfilet serviert mit Quinoa..."
+    },
+    {
+        name: "Thai Curry mit Garnelen",
+        price: 13.75,
+        description: "Scharfes Curry mit Kokosmilch, Garnelen und Gemüse..."
+    }
+];
+
+window.dishes = dishes;
