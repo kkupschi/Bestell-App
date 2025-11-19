7e2c1f74f28f61da8c2c461979857e7b400f894c
kkupschi <kkupschi@gmail.com>
2025-11-19 13:10:07 +0100

    Implement cart functionality and update styles for cart items

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let cart = [];
+
 function init() {
     renderDishList();
 }
@@ -25,9 +27,81 @@ function createDishHtml(dish, index) {
   `;
 }
 
-// Platzhalter für später (Warenkorb)
-function addToCart(index) {
-    console.log("Gericht in Warenkorb legen:", dishes[index].name);
+// Warenkorb
+function addToCart(dishIndex) {
+    let existingIndex = findCartItemIndex(dishIndex);
+
+    if (existingIndex === -1) {
+        cart.push({ dishIndex: dishIndex, amount: 1 });
+    } else {
+        cart[existingIndex].amount++;
+    }
+
+    renderCart();
+}
+
+function findCartItemIndex(dishIndex) {
+    for (let i = 0; i < cart.length; i++) {
+        if (cart[i].dishIndex === dishIndex) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+function renderCart() {
+    let cartListElement = document.getElementById("cartList");
+    let cartTotalElement = document.getElementById("cartTotal");
+
+    if (cart.length === 0) {
+        cartListElement.innerHTML = "Noch keine Gerichte im Warenkorb.";
+        cartTotalElement.innerHTML = "";
+        return;
+    }
+
+    cartListElement.innerHTML = "";
+
+    for (let i = 0; i < cart.length; i++) {
+        cartListElement.innerHTML += createCartItemHtml(cart[i], i);
+    }
+
+    let total = calculateCartTotal();
+    cartTotalElement.innerHTML = "Gesamt: " + total.toFixed(2) + " €";
+}
+
+function createCartItemHtml(cartItem, index) {
+    let dish = dishes[cartItem.dishIndex];
+    let lineTotal = dish.price * cartItem.amount;
+
+    return /*html*/ `
+    <div class="cart-item">
+      <div class="cart-item-left">
+        <span class="cart-item-name">${dish.name}</span>
+        <span class="cart-item-amount">${cartItem.amount}x</span>
+      </div>
+      <div class="cart-item-right">
+        <span class="cart-item-price">${lineTotal.toFixed(2)} €</span>
+      </div>
+    </div>
+  `;
+}
+
+function calculateCartTotal() {
+    let sum = 0;
+
+    for (let i = 0; i < cart.length; i++) {
+        let item = cart[i];
+        let dish = dishes[item.dishIndex];
+        sum += dish.price * item.amount;
+    }
+
+    return sum;
+}
+
+function init() {
+    renderDishList();
+    renderCart();
 }
 
 init();
